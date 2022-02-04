8f9bd8437a0114e03306a68d884b5e2d656dfc99
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-02-04 17:27:39 -0500

    got promo code working on cart

--- frontend/src/components/cart/CartCheckoutInfo.js
+++ frontend/src/components/cart/CartCheckoutInfo.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import CheckOutBtn from './CheckOutBtn';
 // import Axios from 'axios';
 
 export default function CartCheckoutInfo({ subtotal, taxes, total }) {
+  const [promoTotal, setPromoTotal] = useState(1);
+
+  const disabled = promoTotal === 1 ? false : true;
+  const savingsDrop = promoTotal === 1 ? 'close-savings' : 'open-savings';
+  // const savings = total - promoTotal;
+
+  // const finalTotal = promoTotal ? promoTotal : total;
+
   return (
     <div className="cart-total-container flex-col center-items">
       <div className="cart-total-subcontainer flex-col">
         <Subtotal subtotal={subtotal} />
         <Taxes taxes={taxes} />
         <span className="login-line cart-line"></span>
-        <Total total={total} />
-        <PromoCode />
+        <Total
+          discount={promoTotal}
+          savingsDrop={savingsDrop}
+          currentTotal={total}
+        />
+        <PromoCode
+          setPromoTotal={setPromoTotal}
+          currentTotal={total}
+          disabled={disabled}
+        />
         <CheckOutBtn />
       </div>
     </div>
@@ -35,27 +51,57 @@ function Taxes({ taxes }) {
   );
 }
 
-function Total({ total }) {
+function Total({ discount, savingsDrop, currentTotal }) {
+  const currentDiscount = discount !== 1 ? currentTotal * discount : 0;
+
+  const finalTotal = currentTotal - currentDiscount;
+
   return (
     <div className="cart-total">
       <span>TOTAL</span>
-      <span className="amount-txt">{`$${total}`}</span>
+      <span className="amount-txt">{`$${finalTotal.toFixed(2)}`}</span>
+      <div className={`savings ${savingsDrop}`}>
+        <span>Saved</span>
+        <span>{`$${currentDiscount.toFixed(2)}`}</span>
+      </div>
     </div>
   );
 }
 
-function PromoCode() {
+function PromoCode({ setPromoTotal, currentTotal, disabled }) {
+  const promoRef = useRef();
+
+  const promoCodes = {
+    code: 0.01,
+    cenimas: 0.1,
+    mypromo: 0.2,
+    discount: 0.5,
+  };
+
+  function handleClick(event) {
+    event.preventDefault();
+    if (promoRef.current.value in promoCodes) {
+      // const discount = currentTotal * promoCodes[promoRef.current.value];
+
+      setPromoTotal(promoCodes[promoRef.current.value]);
+      promoRef.current.value = '';
+    }
+  }
   return (
     <div className="cart-promo-code">
-      <form>
+      <form onClick={handleClick}>
         <label htmlFor="promo-code"></label>
         <input
-          defaultValue="PROMO CODE"
+          placeholder="PROMO CODE"
           type="text"
           name="promo-code"
           id="promo-code"
+          ref={promoRef}
+          disabled={disabled}
         ></input>
-        <button className="promo-btn">ADD</button>
+        <button disabled={disabled} type="submit" className="promo-btn">
+          ADD
+        </button>
       </form>
     </div>
   );
