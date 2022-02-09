cccb63c0db140d95201e3585b890a9f402e0bf61
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-02-09 04:09:00 -0500

    now trying to solve  redis problem

--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -11,8 +11,8 @@ const redisClient = redis.createClient({
   },
 });
 
-// redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-// await redisClient.connect();
+await redisClient.connect();
 
 export default redisClient;
