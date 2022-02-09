b1c4ee67f1797c7f656a22f6b035e71e5b6525e8
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-02-09 04:11:26 -0500

    changing the redis config

--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -5,14 +5,10 @@ import redis from 'redis';
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+// redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-await redisClient.connect();
+// await redisClient.connect();
 
 export default redisClient;
