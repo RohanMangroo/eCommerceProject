daf40f28b9478ba0c7d74e58a20e1d04f9abc452
Seeram Rohan Mangroo <blue22387@gmail.com>
2023-01-05 19:05:41 -0500

    added render db config

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,14 @@
 import db from 'pg';
 const Pool = db.Pool;
 
+const pool = new Pool({
+  user: 'rohan',
+  password: 'qm8HuakdTZjTPiRW2zegrhT1iOydXb12',
+  database: 'musedb',
+  host: 'dpg-cebtp4kgqg4ap4a54co0-a',
+  port: '5432',
+});
+
 // const pool = new Pool({
 //   user: 'rohan',
 //   password: 'Mastodon0d',
@@ -9,12 +17,12 @@ const Pool = db.Pool;
 //   port: '5432',
 // });
 
-const pool = new Pool({
-  // create connection to database
-  connectionString: process.env.DATABASE_URL, // use DATABASE_URL environment variable from Heroku app
-  ssl: {
-    rejectUnauthorized: false, // don't check for SSL cert
-  },
-});
+// const pool = new Pool({
+//   // create connection to database
+//   connectionString: process.env.DATABASE_URL, // use DATABASE_URL environment variable from Heroku app
+//   ssl: {
+//     rejectUnauthorized: false, // don't check for SSL cert
+//   },
+// });
 
 export default pool;
